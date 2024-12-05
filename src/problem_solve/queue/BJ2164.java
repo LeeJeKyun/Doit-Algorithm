394135d918decbe0b10901081b71b340e87edbfe
LeeJeKyun <zxsi2003@naver.com>
2024-12-05 16:55:40 +0900

    2164품

---
+++ src/problem_solve/queue/BJ2164.java
@@ -0,0 +1,23 @@
+package problem_solve.queue;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BJ2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i=1; i<=N; i++) {
+            queue.add(i);
+        }
+        while(queue.size() != 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
